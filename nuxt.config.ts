95ba47ac7d8547311e52324c44c579eb5f054fa0
Hamza-b93 <hamza.b93@protonmail.com>
2024-12-10 16:31:32 +0500

    Updated Build.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts',
-    '@nuxt/icon'
-  ]
-})
+  app: {
+    baseURL: "/my-portfolio-app/", // Replace <REPO_NAME> with your repository name
+  },
+  nitro: {
+    preset: "github-pages",
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/icon"],
+});
