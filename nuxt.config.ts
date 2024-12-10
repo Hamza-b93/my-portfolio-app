e796c173919720f2018455ce771b2bd6b4e21ac6
Hamza-b93 <hamza.b93@protonmail.com>
2024-12-10 17:10:48 +0500

    Updated Configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  app: {
-    baseURL: "/my-portfolio-app/", // Replace <REPO_NAME> with your repository name
-  },
-  nitro: {
-    preset: "github-pages",
-  },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/icon"],
 });
